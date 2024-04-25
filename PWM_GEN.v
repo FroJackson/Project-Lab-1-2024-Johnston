bc919dba77206dcf83dc7ca55ead1ee7eb2eb1a5
Jackson_Blackmon <158416965+FroJackson@users.noreply.github.com>
2024-04-25 18:33:39 -0500

    Update PWM_GEN.v

--- PWM_GEN.v
+++ PWM_GEN.v
@@ -3,47 +3,42 @@ module PWM_Generator_Find(
     output reg PWM_R, PWM_L
 );
 
-    reg [19:0] width_80;         // Pulse Width
-    reg [19:0] width_40;
-    reg [19:0] width_90;
-    reg [19:0] count;
-    initial begin            // Initialize Registers to Zero
-        width_80 = 20'd800000;
-        width_40 = 20'd400000;
-        width_90 = 20'd900000;
-        count = 0;
-    end
+    reg [19:0] width_65 = 20'd650000;         // Pulse Width
+    reg [19:0] width_25 = 20'd300000;
+    reg [19:0] width_90 = 20'd900000;
+    reg [19:0] width_50 = 20'd500000;
+    reg [19:0] count = 0;
 
     always@(posedge clk) begin
 
         count <= count + 1;
 
     if(~RvL)begin
-        if(count < width_40) begin
-            PWM_L = 1;
+        if(count < width_25) begin
+            PWM_R = 1;
             end
         else begin
-            PWM_L = 0;
+            PWM_R = 0;
             end
         if(count < width_90) begin
-            PWM_R = 1;
-            end
-        else begin
-            PWM_R = 0;
-            end   
-    end
-    else begin
-        if(count < width_80) begin
             PWM_L = 1;
             end
         else begin
             PWM_L = 0;
-            end
-        if(count < width_40) begin
+            end   
+    end
+    else if (RvL) begin
+        if(count < width_50) begin
             PWM_R = 1;
             end
         else begin
             PWM_R = 0;
+            end
+        if(count < width_90) begin
+            PWM_L = 1;
+            end
+        else begin
+            PWM_L = 0;
             end  
     end
     end
